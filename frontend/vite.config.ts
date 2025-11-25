60cdcb32e2a66adfe3d6948ce0e824b034c107e8
white-fortune <whitefortune271828@gmail.com>
2025-11-25 20:40:43 +0600

    updated tsconfig and vite to resolve @utils and @icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
 		alias: {
 			"@components": path.resolve(__dirname, "src/components"),
 			"@contexts": path.resolve(__dirname, "src/contexts"),
+			"@utils": path.resolve(__dirname, "src/utils"),
 			"@hooks": path.resolve(__dirname, "src/hooks"),
 			"@stypes": path.resolve(__dirname, "src/types"),
 			"@images": path.resolve(__dirname, "src/assets/images"),
+			"@icons": path.resolve(__dirname, "src/assets/icons"),
 			"@shared-types": path.resolve(__dirname, "../types"),
 		}
 	},
